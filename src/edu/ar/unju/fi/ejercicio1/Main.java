833b753ab6e504401a28c686a1e1de185b41bed6
Darkos0912 <villanuevadarkos@gmail.com>
2024-04-05 19:13:27 -0300

    Ejercicio 1 - Esteban Villanueva

--- src/edu/ar/unju/fi/ejercicio1/Main.java
+++ src/edu/ar/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,17 @@ package edu.ar.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1 = 4;
+		int num2 = 23;
+		int num3 = 6;
+		int num4 = 2;
+		int num5 = 6;
+		
+		double promedio = (double) (num1 + num2 + num3 + num4 + num5) / 5;
+		
+		System.out.println("Valores ingresados: " + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + ".");
+		System.out.println("El promedio resultante es: " + promedio);
+		
 	}
 
 }
