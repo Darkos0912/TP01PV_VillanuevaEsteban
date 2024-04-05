2d3797aaf2afe98fe20e453f561ed90f0e84317d
Darkos0912 <villanuevadarkos@gmail.com>
2024-04-05 19:29:34 -0300

    Ejercicio 2 - Esteban Villanueva

---
+++ src/edu/ar/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,20 @@
+package edu.ar.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Argentina";
+		int edad = 24;
+		double alturaEdificio = (double)300.6;
+		double precioProducto = (double) 200.50;
+		long telefono = 3884580983l;
+		double resultado = Math.cos(0.5);
+		
+		System.out.println("El nombre del país es: " + pais);
+		System.out.println("La edad es: " + edad);
+		System.out.println("La altura del edifcio es: " + alturaEdificio);
+		System.out.println("El precio del producto es: " + precioProducto);
+		System.out.println("El telefono es: " + telefono);
+		System.out.println("El resultado del coseno de 0.5 es: " + resultado);
+	}
+}
